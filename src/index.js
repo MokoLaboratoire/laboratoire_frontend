f478dab7b5eee83a85a0058478098ccddb4766a6
Raoul de Godewarsvelde <raouldegodewarsvelde.thetrueone@gmail.com>
2023-01-30 19:37:35 +0100

    BASE: routing createBrowserRouter + RouterProvider + HomePage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import HomePage from './Pages/HomePage';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
