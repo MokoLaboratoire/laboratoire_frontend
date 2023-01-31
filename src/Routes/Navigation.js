12f5f9ba719831abf49046059aa17f706d6bf067
Federico <federico.desmoulin@lento.eu>
2023-01-31 14:14:43 +0100

    CLEAN: unused import removed

--- src/Routes/Navigation.js
+++ src/Routes/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createBrowserRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import '../Styles/Routes/Navigation.css'
 
 const Navigation = () => {
